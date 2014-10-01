15ee6e8d4c0c1153d2fa7a156265575b87b2d4c1
idsc3101 <idsc3101@gmail.com>
2014-10-01 15:26:26 -0500

    5

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -5,11 +5,21 @@ Public Class Form1
     Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
         Dim name As String
         name = txtFullName.Text
+        'name = txtFullName.Text.Trim() this will allow you to put spaces in front of first name 
         txtFirstName.Text = FirstName(name)
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        Dim idxSpace As Integer
+        idxSpace = name.IndexOf(" ")
+
+        Dim fstName As String
+        fstName = name.Substring(0, idxSpace)
+
+        Return fstName
     End Function
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
